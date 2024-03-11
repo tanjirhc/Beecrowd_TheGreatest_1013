06de2519b4f3160420e01a3c5fd5977f3adfef6b
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-11 21:51:16 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beecrowd_TheGreatest_1013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            int A = Convert.ToInt32(valores[0]);
+            int B = Convert.ToInt32(valores[1]);
+            int C = Convert.ToInt32(valores[2]);
+
+            int maiorAB = (A + B + Math.Abs(A - B)) / 2;
+            int maiorMaiorC = (maiorAB + C + Math.Abs(maiorAB - C)) / 2;
+
+            Console.WriteLine($"{maiorMaiorC} eh o maior");
+            Console.ReadKey();
+        }
+    }
+}
